fd49beac138fcc9618a4cb4feb1080a3058fc7d6
Xavi11233 <159134334+Xavi11233@users.noreply.github.com>
2024-04-16 14:56:42 +0100

    Update sched-group1.py
    skibidi Ohio w rizz fanum tax

--- sched-group1.py
+++ sched-group1.py
@@ -1,22 +1,6 @@
 import random
 import matplotlib.pyplot as plt
 
-class Appliance():
-    """
-    This class simulates a household appliance. 
-    """
-    def __init__(self, name, phases):
-        self.name = name
-        self.phases = phases
-        self.scheduleLength = len(phases)
-
-    def __repr__(self):
-        return f"This appliance is a {self.name} and  has a schedule length of {self.scheduleLength}\nThe schedule is "
-class Timings():
-    def __init__(self, ):
-        pass
-
-
 def cost(S, N, X):
     # Return the cost of solution S with respect to N and X
     assert len(S) == len(N) 
@@ -29,19 +13,52 @@ def cost(S, N, X):
     return total
 
 # Main Program: Ask the user to specify the input file, then read it into memory.
+infile = input("Enter problem file name >> ")
+with open(infile, 'r') as f:
+    N = eval(f.readline())
+    X = eval(f.readline())
 
-def open_file(infile):
-    
-    with open(infile, 'r') as f:
-        N = eval(f.readline())
-        X = eval(f.readline())
-    # Do some basic checks to make sure the input is valid
-    assert type(N) == list and type(X) == list and len(N) >= len(X), "Error. Invalid input"
-    n = len(N)
-    x = len(X)
-    return N,n,X,x
+# Do some basic checks to make sure the input is valid
+assert type(N) == list and type(X) == list and len(N) >= len(X), "Error. Invalid input"
+n = len(N)
+x = len(X)
+
+# Make a random solution
+S = []
+for i in range(x):
+    S.append(1)
+for i in range(n-x):
+    S.append(0)
+random.shuffle(S)
+
+# Write some information to the screen
+#print("File    =", infile)
+#print("n       =", n)
+#print("x       =", x)
+#print("N       =", N)
+#print("X       =", X)
+#print("S       =", S)
+#print("cost(S) =", cost(S, N, X))
+ListOfCosts = []
+for i in range(100000):
+    S = []
+    for i in range(x):
+        S.append(1)
+    for i in range(n-x):
+        S.append(0)
+    random.shuffle(S)
+    ListOfCosts.append(cost(S, N, X))
 
 
+ListOfCosts = sorted(ListOfCosts)
+NoDuplicatesListOfCosts = sorted(list(set(ListOfCosts)))
+FrequencyOfCosts = []
+for i in NoDuplicatesListOfCosts:
+    FrequencyOfCosts.append(ListOfCosts.count(i))
+print(min(NoDuplicatesListOfCosts))
 
-myWasher = Appliance("Washing machine", [4,6,0,1])
-print(myWasher.hi)
+plt.xlabel("Cost")
+plt.ylabel("Frequency")
+plt.bar(NoDuplicatesListOfCosts, FrequencyOfCosts, color = 'cyan')
+plt.plot(NoDuplicatesListOfCosts, FrequencyOfCosts, color = 'red')
+plt.show()
